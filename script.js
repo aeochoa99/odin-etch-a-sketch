d36292e7109ac310312738f8f7a8fc0dda4bec33
anthonyochoa99 <aeochoa99@outlook.com>
2026-07-28 23:38:41 -0700

    Add logic to allow custom grid size creation

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 const gridContainer = document.querySelector(".grid");
 const clearButton = document.querySelector("#clear");
+const createGridButton = document.querySelector("#create");
 
-function createGrid() {
-    for (let i = 0; i < 256; i++) {
+function createGrid(gridSize) {
+    gridContainer.replaceChildren();
+    
+    const numOfCells = gridSize * gridSize;
+
+    for (let i = 0; i < numOfCells; i++) {
         const gridCell = document.createElement("div");
-        gridCell.style.width = "calc(100% / 16)";
-        gridCell.style.height = "calc(100% / 16)";
+        gridCell.style.width = `calc(100% / ${gridSize})`;
+        gridCell.style.height = `calc(100% / ${gridSize})`;
         gridContainer.appendChild(gridCell);
     }
-}
 
-createGrid();
+    const cells = document.querySelectorAll(".grid > div");
 
-const cells = document.querySelectorAll(".grid > div");
-
-cells.forEach(cell => {
+    cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {
         cell.style.backgroundColor = "#3C3C3C";
     })
 })
+}
 
 function clearGrid() {
+    const cells = document.querySelectorAll(".grid > div");
     cells.forEach(cell => {
         cell.style.backgroundColor = "#D9D9D9";
     })
 }
 
 clearButton.addEventListener("click", clearGrid);
+createGridButton.addEventListener("click", () => {
+    const getGridSize = Number(prompt("Enter a number for your grid size. (Max of 100)"));
+
+    if (isNaN(getGridSize)) {
+        alert("Please enter a valid number.");
+    } else {
+        createGrid(getGridSize);
+    }
+})
+
+createGrid(16);
